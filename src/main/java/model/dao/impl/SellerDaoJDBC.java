c2ecaa57f715cb8296aae69ec4e5c7f50c475e4d
Marthiins <sergio.marthiins@gmail.com>
2022-03-05 14:44:44 -0300

    findAll implementation

--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -91,10 +91,50 @@ public class SellerDaoJDBC implements SellerDao {
 		return dep;
 	}
 
+	
+	/*findAll faz buscar todos os vendedores com nome de departamentos*/
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try { /* Iniciando o PreparedStatement */
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+		
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();/*Estrutura Map vazio*/
+			
+			while (rs.next()) {/*Meu resultado pode ter 0 ou mais valores*/
+			
+				/*Criei o map la em cima vazio, vou guardar 
+				 * dentro do Map qualquer departamento que eu instanciar
+				 * e a cada vez que passar pelo while vou testar
+				 * se o departamento já existe.
+				 * Como é feito isso vou no map.get e buscar um departamento que tem o rs.getInt("DepartmentId" */
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {/*Teste*/
+				dep  = instantiateDepartment(rs);
+				map.put(rs.getInt("DepartmentId"), dep);/*Salvar o departamento dentro do MAP*/
+				}
+
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+
+			}
+			return list;
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
